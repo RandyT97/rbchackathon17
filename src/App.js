34c54fcab8cdf486eba2f74066efd7b7264f7446
Jawyuhz <rtsui9797@gmail.com>
2017-09-30 13:27:28 -0400

    Added google charts html dependency

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import 'whatwg-fetch';
 import './App.css';
 
 class App extends Component {
-
+    render() {
+        return (
+            <h1>Camilo sucks</h1>
+        )
+    }
+}
+function Button() {
+    return (
+        React.createElement("button",null,"Go")
+    );
 }
 
 export default App;

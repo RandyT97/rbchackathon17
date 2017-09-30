38f3836a3b3bf3927c4b6b479c8b35557706b3f1
Jawyuhz <rtsui9797@gmail.com>
2017-09-30 16:57:14 -0400

    rename piceChart.css to pieChart.css

--- src/pieChart.js
+++ src/pieChart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
-import './piceChart.css';
+import './pieChart.css';
 import { Chart } from 'react-google-charts';
 
 class App extends Component {

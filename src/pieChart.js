840a401c5d0e122e723ba26c5985a5e28264acac
Jawyuhz <rtsui9797@gmail.com>
2017-09-30 14:43:43 -0400

    Changed render function to pieChart

---
+++ src/pieChart.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+import './App.css';
+import { Chart } from 'react-google-charts';
+
+class App extends Component {
+
+    render() {
+        return(
+            <div className={'chart-container'}>
+                <Chart
+                    chartType="PieChart"
+                    data={[["Type", "Dollars"],
+                        ["randy", 20],
+                        ["camilo", 5]]}
+                    title="Spending"
+                    is3d="true"
+                />
+            </div>
+        );
+    }
+
+}
+
+export default App;

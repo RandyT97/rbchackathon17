f845689a86a28cdd1947243ed1555a60c8524926
jawyuhz <rtsui9797@gmail.com>
2017-09-30 23:56:04 -0400

    TEST

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pieChart';
+import pieChart from './pieChart';
 import registerServiceWorker from './registerServiceWorker';
 import 'whatwg-fetch';
 
